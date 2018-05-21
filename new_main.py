c38baa76a8a0d658244cb208439cd7e7381e9008
Tianxiang Chen <tchen@pindropsecurity.com>
2018-05-21 20:05:22 +0000

    test on 2018 data

--- new_main.py
+++ new_main.py
@@ -16,8 +16,7 @@ config.gpu_options.allow_growth=True
 sess = tf.Session(config=config)
 
 from utils import load_train_weak
-
-
+from train_utils import base_model_1, benchmark_model
 from sklearn.preprocessing import MultiLabelBinarizer
 mlb = MultiLabelBinarizer()
 
@@ -27,74 +26,6 @@ batch_size = 32
 # Create audio generator
 audio_gen = DataGenerator(data_list=(X_train_fn, y_train_one_hot))
 
-def global_average_pooling(x):
-    return K.mean(x, axis = (3))
-
-def global_average_pooling_shape(input_shape):
-    return input_shape[0:3]
-
-def average_pooling(inputs, **kwargs):
-    input = inputs[0]   # (batch_size, time_steps, freq_bins)
-    return K.mean(input, axis=1)
-
-
-def max_pooling(inputs, **kwargs):
-    input = inputs[0]   # (batch_size, time_steps, freq_bins)
-    return K.max(input, axis=1)
-
-
-def attention_pooling(inputs, **kwargs):
-    [out, att] = inputs
-
-    epsilon = 1e-7
-    att = K.clip(att, epsilon, 1. - epsilon)
-    normalized_att = att / K.sum(att, axis=1)[:, None, :]
-
-    return K.sum(out * normalized_att, axis=1)
-
-
-def pooling_shape(input_shape):
-
-    if isinstance(input_shape, list):
-        (sample_num, time_steps, freq_bins) = input_shape[0]
-
-    else:
-        (sample_num, time_steps, freq_bins) = input_shape
-
-    return (sample_num, freq_bins)
-
-
-def base_model_1():
-
-    input_layer = Input(shape=(image_shape[1], image_shape[2], 1))
-    
-    cnn = Conv2D(64, (3, 3), padding='valid')(input_layer)
-    cnn = Activation('relu')(cnn)
-    #cnn = MaxPooling2D((1, 4))(cnn)
-    cnn = Conv2D(64, (3, 3), padding='valid')(cnn)
-    cnn = Activation('relu')(cnn)
-    #cnn = MaxPooling2D((1, 4))(cnn)
-    cnn = Conv2D(64, (3, 3), padding='valid')(cnn)
-    cnn = Activation('relu')(cnn)
-    cnn = Conv2D(10, (493, 34), padding='valid')(cnn)
-    cnn = Activation('relu')(cnn)
-    cnn = Reshape((1,10))(cnn)
-    dense_a = Dense(128, activation='relu')(cnn)
-    #dense_a = Lambda(global_average_pooling,output_shape=global_average_pooling_shape)(cnn)
-    dense_b = Dense(128, activation='relu')(dense_a)
-    cla = Dense(128, activation='linear')(dense_a)
-    att = Dense(128, activation='sigmoid')(dense_a)
-    dense_b = Lambda(attention_pooling, output_shape=pooling_shape)([cla, att])
-    b1 = BatchNormalization()(dense_b)
-    b1 = Activation(activation='relu')(b1)
-    b1 = Dropout(dropout_rate)(b1)
-    #b2 = Dense(512)(b1)
-    #b2 = BatchNormalization()(b1)
-    #b2 = Activation(activation='relu')(b2)
-    #b2 = Dropout(dropout_rate)(b2)
-    output_layer = Dense(classes_num, activation='sigmoid')(b1)
-    model = Model(inputs=input_layer, outputs=output_layer)
-    print (model.summary())
 
 classes_num = 10
 dropout_rate = 0.25
@@ -107,71 +38,8 @@ step_per_epoch = num_train // batch_size
 validation_step = num_test // batch_size
 image_shape = Sxx.shape
 print(image_shape)
-'''
-input_layer = Input(shape=(image_shape[1], image_shape[2], 1))
-cnn = Conv2D(64, (3, 3), padding='same')(input_layer)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((1, 5))(cnn)
-cnn = Conv2D(64, (3, 3), padding='same')(cnn)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((1, 4))(cnn)
-cnn = Conv2D(64, (3, 3), padding='same')(cnn)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((1, 2))(cnn)
-cnn = Reshape((499,64))(cnn)
-
-bi_gru = Bidirectional(GRU(128, activation='tanh', dropout=dropout_rate, recurrent_dropout=dropout_rate,return_sequences=True))(cnn)
-bi_gru = Bidirectional(GRU(128, activation='tanh', dropout=dropout_rate, recurrent_dropout=dropout_rate,return_sequences=True))(bi_gru)
-dense_a = TimeDistributed(Dense(10, activation='relu'))(bi_gru)
-dense_a = Dropout(rate=dropout_rate)(dense_a)
-dense_a = TimeDistributed(Dense(10, activation='sigmoid'))(dense_a)
-
-# Weak output path
-weak_dense_a = TimeDistributed(Dense(1, activation='sigmoid'))(dense_a)
-flat = Flatten()(weak_dense_a)
-weak_dense_b = Dense(32, activation='relu')(flat)
-Dropout(rate=dropout_rate)
-weak_out = Dense(classes_num, activation='sigmoid')(weak_dense_b)
-
-# Strong output path
-strong_out = TimeDistributed(Dense(classes_num, activation='sigmoid'))(dense_a)
-model = Model(inputs=input_layer, outputs=weak_out)
-# model = Model(inputs=input_layer, outputs=[weak_out, strong_out])
-print (model.summary())
-'''
-
 # Attention CNN
-
-input_layer = Input(shape=(image_shape[1], image_shape[2], 1))
-
-cnn = Conv2D(64, (3, 3), padding='valid')(input_layer)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((1, 2))(cnn)
-cnn = Conv2D(64, (3, 3), padding='valid')(cnn)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((1, 2))(cnn)
-cnn = Conv2D(64, (3, 3), padding='valid')(cnn)
-cnn = Activation('relu')(cnn)
-cnn = MaxPooling2D((2, 1))(cnn)
-#cnn = Conv2D(10, (493, 34), padding='valid')(cnn)
-#cnn = Activation('relu')(cnn)
-cnn = Reshape((246,34*64))(cnn)
-bi_gru = LSTM(512, activation='tanh', recurrent_dropout=dropout_rate,return_sequences=True)(cnn)
-dense_a = Dense(512, activation='relu')(bi_gru)
-#dense_a = Lambda(global_average_pooling,output_shape=global_average_pooling_shape)(cnn)
-dense_b = Dense(256, activation='relu')(dense_a)
-cla = Dense(256, activation='linear')(dense_a)
-att = Dense(256, activation='sigmoid')(dense_a)
-dense_b = Lambda(attention_pooling, output_shape=pooling_shape)([cla, att])
-b1 = BatchNormalization()(dense_b)
-b1 = Activation(activation='relu')(b1)
-b1 = Dropout(dropout_rate)(b1)
-b2 = Dense(128)(b1)
-b2 = BatchNormalization()(b1)
-b2 = Activation(activation='relu')(b2)
-b2 = Dropout(dropout_rate)(b2)
-output_layer = Dense(classes_num, activation='sigmoid')(b2)
-model = Model(inputs=input_layer, outputs=output_layer)
+model = benchmark_model(image_shape, classes_num, dropout_rate)
 print (model.summary())
 
 
@@ -182,11 +50,11 @@ model.compile(optimizer='Adam', loss=losses.binary_crossentropy)
 model.fit_generator(generator=audio_gen.next_train(), steps_per_epoch=step_per_epoch,
                           epochs=40, validation_data=audio_gen.next_test(), validation_steps=validation_step,
                           verbose=1)
-model.save('models/event_detect_attention_3.h5')
+model.save('models/model_2018.h5')
 
 
 
-model = load_model('models/event_detect_attention_3.h5')
+model = load_model('models/model_2018.h5')
 # X_test, y_test = audio_gen.next_test()
 
 from evaluation_measures import get_f_measure_by_class
